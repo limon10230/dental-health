b6aadf061158fc474ba0a943a23a80833e5f3aca
limon10230 <limon.dew121@gmail.com>
2021-10-18 20:39:43 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import('./Header.css')
+
+const Header = () => {
+    return (
+        <div className="header-container ">
+            <div>
+
+                <Container >
+                    <Row>
+                        <Col><h2 style={{ color: 'goldenrod', fontSize: '40px' }}>Learn Quran</h2></Col>
+                        <Col ><nav >
+
+                            <NavLink className="navlink"
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "lightyellow"
+                                }}
+
+                                to="/home">Home</NavLink>
+                            <NavLink className="navlink"
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "lightyellow",
+
+                                }}
+                                to="/about">About us</NavLink>
+                            <NavLink className="navlink"
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "lightyellow"
+                                }}
+                                to="/services"
+                            >Services</NavLink>
+
+
+
+                            <NavLink
+
+                                className="navlink"
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "lightyellow"
+                                }}
+                                to="/books">Books</NavLink>
+                            <NavLink className="navlink"
+                                activeStyle={{
+                                    fontWeight: "bold",
+                                    color: "lightyellow"
+                                }}
+                                to="/contact">Contact Us</NavLink>
+
+                        </nav>
+                        </Col>
+                    </Row>
+
+                </Container>
+
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Header;
